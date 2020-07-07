70693456a801a87f7037bd07d693f9d8bfe94682
Shintaro Noguchi <noguchi.shintaro@his-world.com>
2020-07-07 17:44:45 +0900

    getとpostを追加

--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
+	"sampleRestApp/handler"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
-func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		db, err := gorm.Open("mysql", "user:password@(mysql:3306)/sample?parseTime=true")
-		if err != nil {
-			panic("データベースへの接続に失敗しました")
-		}
-		defer db.Close()
+type user struct {
+	gorm.Model
+	Name string
+	Age  int
+}
 
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+func main() {
+	db, err := gorm.Open("mysql", "user:password@(mysql:3306)/sample?parseTime=true")
+	if err != nil {
+		panic("データベースへの接続に失敗しました")
+	}
+	defer db.Close()
+
+	// usersテーブルがなかった場合、マイグレーションを実行
+	if db.HasTable(&user{}) == false {
+		db.AutoMigrate(&user{})
+	}
+
+	r := gin.Default()
+
+	r.GET("/users", handler.PostUser())
+	r.POST("/users", handler.GetUser())
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
