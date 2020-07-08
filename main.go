7a662691db48af76cecb4f77303fd15672e06938
Shintaro Noguchi <noguchi.shintaro@his-world.com>
2020-07-08 14:58:53 +0900

    サーバの起動処理をmain関数内に移動

--- main.go
+++ main.go
@@ -10,17 +10,19 @@ import (
 )
 
 func main() {
-	cleanup, err := initialize()
+	router, cleanup, err := initialize()
 	if err != nil {
 		fmt.Printf("server start failed. %v", err)
 	}
 	defer cleanup()
+
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
-func initialize() (func(), error) {
+func initialize() (*gin.Engine, func(), error) {
 	repository, cleanup, err := db.NewRepository()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	userHandler := handler.NewUserHandler(repository)
 
@@ -29,7 +31,6 @@ func initialize() (func(), error) {
 
 	r.GET("/users", userHandler.GetAllUser)
 	//r.POST("/users", handler.PostUser())
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 
-	return cleanup, nil
+	return r, cleanup, nil
 }
